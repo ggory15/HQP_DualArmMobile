beaed699677583d90abe88db8ef728a25d0baf91
ggory15 <ggory15@snu.ac.kr>
2018-07-18 19:18:50 +0900

    upload mobilemanipulator demo

--- build-rel/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ggory15/TSID_project
-# Build directory: /home/ggory15/TSID_project/build-rel
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(src)
--- build-rel/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ggory15/TSID_project/src
-# Build directory: /home/ggory15/TSID_project/build-rel/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
